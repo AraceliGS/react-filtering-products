ef9b8111f776f0a7d05d5a1c0b3f4498c9c7f461
AraceliGS <aragutarras@gmail.com>
2018-03-18 20:53:37 -0500

    modificando el archivo reducer.js

--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -1,3 +1,5 @@
+import {actionTypes} from './actions';
+
 const PRODUCTS = [
     { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
     { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
@@ -8,15 +10,54 @@ const PRODUCTS = [
 ];
 
 const INIT_STATE = {
+  originalProducts: PRODUCTS,
   filteredProducts: PRODUCTS,
   filterText: '',
   inStockOnly: false 
 };
 
 export default (state = INIT_STATE, action) => {
-  // debugger;
   switch(action.type) {
+    case actionTypes.FILTER_TEXT_CHANGED:
+    state = {
+      ...state,
+      filterText: action.text
+    }
+    break;
+  case actionTypes.IN_STOCK_ONLY_CHANGED:
+    state = {
+      ...state,
+      inStockOnly: action.value
+    }
+    break;
     default:
       return state;
   }
+
+  if (
+    action.type === actionTypes.FILTER_TEXT_CHANGED ||
+    action.type === actionTypes.IN_STOCK_ONLY_CHANGED
+  ) {
+    const filteredProducts = state.originalProducts.filter(p => {
+      const filterText =
+        (action.type === actionTypes.FILTER_TEXT_CHANGED ?
+          action.text :
+          state.filterText).trim()
+      const inStockOnly =
+        action.type === actionTypes.IN_STOCK_ONLY_CHANGED ?
+          action.value :
+          state.inStockOnly
+
+      return (
+        (inStockOnly ? p.stocked : true) &&
+        p.name.match(new RegExp(filterText, 'ig'))
+      )
+    })
+
+    state = {
+      ...state,
+      filteredProducts
+    }
+  }
+  return state;
 };
